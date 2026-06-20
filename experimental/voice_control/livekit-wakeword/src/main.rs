b163b58168732b5d77365a136125e96a442c758b
Tyler <tylerrmoore@gmail.com>
2026-06-20 14:33:43 -0700

    Working on livekit testing

--- experimental/voice_control/livekit-wakeword/src/main.rs
+++ experimental/voice_control/livekit-wakeword/src/main.rs
@@ -1,8 +1,10 @@
-use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
+use cpal::{StreamConfig, traits::{DeviceTrait, HostTrait, StreamTrait}};
 use livekit_wakeword::wakeword::WakeWordModel;
 
 const SAMPLE_RATE:usize=16000;
 const CHUNK_TIME_MILLISECONDS:usize=2000;
+const CHANNELS:u16=1;
+const BITS_PER_SAMPLE:u32=16;
 const CHUNK_SIZE:usize=SAMPLE_RATE*CHUNK_TIME_MILLISECONDS;
 
 
@@ -12,24 +14,37 @@ async fn main() {
     let host = cpal::default_host();
     let device = host.default_input_device().expect("Should exist.");
 
-    for config in device.supported_input_configs().iter_mut()
+    let sample_rate_u32=SAMPLE_RATE.try_into().expect("Should convert.");
+
+    for supported_config in device.supported_input_configs().expect("Should get configs")
     {
-        println!("Config: {:?}", config);
+        if supported_config.sample_format().is_int() && 
+            supported_config.sample_format().bits_per_sample()==BITS_PER_SAMPLE &&
+            supported_config.channels() == CHANNELS &&
+            supported_config.min_sample_rate() <= sample_rate_u32 &&
+            supported_config.max_sample_rate() >= sample_rate_u32
+        {
+            println!("Desired config found.");
+            println!("   {:?}",supported_config);
+            break;
+        }
     }
 
-    let config = device.default_input_config().expect("Should have config.");
-    println!("Default input config: {:?}", config);
+    let stream_config:StreamConfig=StreamConfig { channels: CHANNELS, sample_rate: sample_rate_u32, buffer_size: cpal::BufferSize::Default };
 
+    //let config = device.default_input_config().expect("Should have config.");
+    //println!("Default input config: {:?}", config);
+    //let stream_config = config.config();
+    
     let err_fn = move |err| {
         eprintln!("An error occurred on the audio stream: {}", err);
     };
 
-    let stream_config = config.config();
-    let raw_data_buffer:[f32;CHUNK_SIZE];
-    let data_fn = move |data: &[f32], _: &cpal::InputCallbackInfo| {
-        // Here, 'data' is a slice of floating-point audio samples
-        // You can process them in real-time or send them over an async channel
-        println!("Captured {} audio samples", data.len());
+    let mut model = WakeWordModel::new(&["/home/tyler/repos/faux-show/experimental/voice_control/livekit-wakeword/hey_livekit.onnx"], SAMPLE_RATE.try_into().expect("Should convert.")).expect("model should start");
+
+    let raw_data_buffer_index:usize=0;
+    let raw_data_buffer:[i16;CHUNK_SIZE];
+    let data_fn = move |data: &[i16], _: &cpal::InputCallbackInfo| {
     };
 
     let stream = device.build_input_stream(
@@ -40,10 +55,6 @@ async fn main() {
     ).expect("Failed to build input stream");
 
     stream.play().expect("Failed to start stream");
-
-    std::thread::sleep(std::time::Duration::from_secs(10));
-    
-    let mut model = WakeWordModel::new(&["/home/tyler/repos/faux-show/experimental/voice_control/livekit-wakeword/hey_livekit.onnx"], SAMPLE_RATE.try_into().expect("Should convert.")).expect("model should start");
 
     // Feed ~2s PCM audio chunks (i16, at configured sample rate)
     //let scores = model.predict(&audio_chunk)?;
