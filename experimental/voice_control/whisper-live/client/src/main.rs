e4726c8cca34367a17c1e20b7bd86981baa94870
Tyler <tylerrmoore@gmail.com>
2026-06-26 22:24:08 -0700

    Working server init

--- experimental/voice_control/whisper-live/client/src/main.rs
+++ experimental/voice_control/whisper-live/client/src/main.rs
@@ -1,6 +1,6 @@
 use std::time::Duration;
 
-use tokio_tungstenite::connect_async;
+use tokio_tungstenite::{connect_async, tungstenite::Message};
 use futures_util::{SinkExt, StreamExt};
 
 #[tokio::main]
@@ -12,18 +12,15 @@ async fn main() {
 
     let (mut write, mut read) = ws_stream.split();
 
-    let test_send = "{\"uid\": \"test_client\",\"language\": \"en\",\"model\": \"base\",\"use_vad\": false}";
+    let test_send = "{\"uid\": \"test_client\",\"language\": \"en\",\"model\": \"base\",\"use_vad\": false,\"task\":\"live-stt\"}";
 
     println!("{}",test_send);
 
     //write.send(test_send.into()).await.expect("Should complete.");
+
+    let test_message = Message::text(test_send);
     
-    //Seems should just feed and not "send" or "flush" otherwise websocket gets closed...but this doesn't seem to initialize the server.
-    println!("Waiting...");
-    std::thread::sleep(Duration::from_secs(3));
-    println!("Sending...");
-    write.feed(test_send.into()).await.expect("Should complete.");
-    println!("Sent.");
+    write.send(test_message).await.expect("Should complete.");
 
     loop {
         match read.next().await
@@ -33,6 +30,21 @@ async fn main() {
                 {
                     Ok(next)=>{ 
                         println!("Websocket received: {:?}",next);
+                        match next
+                        {
+                            Message::Text(_utf8_bytes) => (),
+                            Message::Binary(_bytes) => (),
+                            Message::Ping(_bytes) => (),
+                            Message::Pong(_bytes) => (),
+                            Message::Close(close_frame) => {
+                                match close_frame
+                                {
+                                    Some(_close_frame)=>(),
+                                    None=>()
+                                }
+                            },
+                            Message::Frame(_frame) => (),
+                        }
                     },
                     Err(e)=>{
                         eprintln!("{:?}",e);
