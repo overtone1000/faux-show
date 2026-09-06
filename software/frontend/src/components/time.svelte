7ac2c8ad56be5833d75a1bfb10139494d1b23b2d
Tyler <tylerrmoore@gmail.com>
2026-09-06 07:33:18 -0700

    Shorten months

--- software/frontend/src/components/time.svelte
+++ software/frontend/src/components/time.svelte
@@ -22,18 +22,18 @@
     });
 
     const months=[
-        "January",
-        "February",
-        "March",
-        "April",
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
         "May",
-        "June",
-        "July",
-        "August",
-        "September",
-        "October",
-        "November",
-        "December"
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec"
     ];
 </script>
 
